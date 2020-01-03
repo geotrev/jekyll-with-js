c1ce860e48f4d03dd37346ea60e15c810d1f3056
george <hello@geotrev.com>
2020-01-03 03:37:38 +0000

    Merge branch 'develop'

--- _scripts/home/home.js
+++
@@ -1,6 +0,0 @@
-// divide will be tree-shaken by rollup, and this comment removed
-import { multiply } from "../modules/_math-helpers"
-
-const product = multiply(3, 8)
-
-console.log(`I did it! Three times eight is ${product}`)
--- _scripts/modules/_math-helpers.js
+++
@@ -1,3 +0,0 @@
-export const multiply = (num1, num2) => num1 * num2
-
-export const divide = (num1, num2) => num1 / num2
